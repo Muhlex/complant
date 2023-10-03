a517e0679716856b165cb67abf0ff25cdc9bdf2c
murlis <21311428+Muhlex@users.noreply.github.com>
2023-10-03 21:48:03 +0200

    Add Svelte webgui

---
+++ webgui/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+let config = {};
+$: configDisplay = JSON.stringify(config, undefined, 2);
+
+async function logConfig() {
+	const response = await fetch("http://192.168.4.1/config");
+	config = await response.json();
+}
+</script>
+
+<main>
+	<h1>Complant</h1>
+	<button on:click={logConfig}>fetch config</button>
+	<pre>{configDisplay}</pre>
+</main>
+
+<style>
+	main {
+		width: 100%;
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 1em;
+	}
+
+	h1 {
+		margin-bottom: 1em;
+	}
+</style>
